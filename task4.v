4c42dd5bbdb8a7c8c3ea5c3e7a5a7447cf3f7cb6
ciny2312 <ciny2312@sjtu.edu.cn>
2024-10-10 21:25:43 +0800

    init

---
+++ task4.v
@@ -0,0 +1,14 @@
+/*
+    Problem:
+    https://acm.sjtu.edu.cn/OnlineJudge/problem?problem_id=1250
+
+    任务：掌握组合逻辑，完成一个加法器。
+*/
+
+module Add(
+    input       [31:0]          a,
+    input       [31:0]          b,
+    output reg  [31:0]          sum
+);
+    
+endmodule
