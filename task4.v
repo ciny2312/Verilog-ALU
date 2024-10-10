9af9808ab8510b0ee10e36fc46951414f09aec77
ciny2312 <ciny2312@sjtu.edu.cn>
2024-10-10 22:07:58 +0800

    down

--- task4.v
+++ task4.v
@@ -10,5 +10,14 @@ module Add(
     input       [31:0]          b,
     output reg  [31:0]          sum
 );
+    integer i;
+    reg c=0;
+    always @(*) begin
+        for(i=0;i<32;i++)begin
+            sum[i]=a[i]^b[i]^c;
+            c=(a[i]&b[i])|(c&a[i])|(c&b[i]);
+        end
+    end
+    // TODO
     
 endmodule
