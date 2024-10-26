7eecaf81294dfbcfafba7f12b7f73dfadd81e78e
ciny2312 <ciny2312@sjtu.edu.cn>
2024-10-27 07:41:39 +0800

    use generate

--- task4.v
+++ task4.v
@@ -1,23 +1,23 @@
-/*
-    Problem:
-    https://acm.sjtu.edu.cn/OnlineJudge/problem?problem_id=1250
-
-    任务：掌握组合逻辑，完成一个加法器。
-*/
-
 module Add(
     input       [31:0]          a,
     input       [31:0]          b,
     output reg  [31:0]          sum
 );
     integer i;
-    reg c=0;
+    wire [32:0] c;
+
+    assign c[0]=1'b0;
+    genvar j;
+    generate
+        for(j=0;j<32;j=j+1)begin
+            assign c[j+1]=(a[j]&b[j])|(c[j]&a[j])|(c[j]&b[j]);
+        end
+    endgenerate
+
     always @(*) begin
-        for(i=0;i<32;i++)begin
-            sum[i]=a[i]^b[i]^c;
-            c=(a[i]&b[i])|(c&a[i])|(c&b[i]);
+        for(i=0;i<32;i=i+1)begin
+            sum[i]=a[i]^b[i]^c[i];
         end
     end
-    // TODO
     
 endmodule
